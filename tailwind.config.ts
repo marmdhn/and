84f64d61d60fc98781ff20c005ba0be52630f04b
marmdhn <makbarramadhan62@gmail.com>
2025-09-22 16:30:38 +0700

    refactor: Remove Tailwind CSS configuration file

--- tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "tailwindcss";
-
-const config: Partial<Config> = {
-  plugins: [],
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {},
-};
-export default config;
