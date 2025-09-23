35a7cb6030251b7303c8fe9c13ddcdc18160f04a
marmdhn <makbarramadhan62@gmail.com>
2025-09-23 15:02:16 +0700

    feat: Add reusable Button component with optional icon support

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import rightArrow from "/icons/right_arrow.svg";
+
+export default function Button({
+  text,
+  icon = false,
+}: {
+  text: string;
+  icon?: boolean;
+}) {
+  return (
+    <div className="bg-secondary p-1 rounded-full flex items-center gap-2 transition duration-300 hover:scale-105 hover:shadow-lg will-change-transform">
+      <button className="btn btn-primary rounded-full border-0 will-change-transform">
+        {text}
+      </button>
+      {icon && <img src={rightArrow} className="w-8" alt="logo" />}
+    </div>
+  );
+}
