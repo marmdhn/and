18cbc8643fe31c0b615377c94d020c9db78f0100
marmdhn <makbarramadhan62@gmail.com>
2025-09-23 16:15:50 +0700

    refactor: Add ServiceSection component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/sections/Hero";
+import ServiceSection from "./components/sections/Service";
 
 function App() {
   return (
     <div data-theme="mytheme" className="px-20">
       <Navbar />
       <HeroSection />
+      <ServiceSection />
     </div>
   );
 }
