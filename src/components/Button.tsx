2beb6adc16a4d9ee284bc8f2111c93f144be3cb5
marmdhn <makbarramadhan62@gmail.com>
2025-09-24 15:47:03 +0700

    refactor: Add onClick prop to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,18 @@
 import { FaCircleArrowRight } from "react-icons/fa6";
 
-export default function Button({
-  text,
-  icon = false,
-}: {
+interface ButtonProps {
   text: string;
   icon?: boolean;
-}) {
+  onClick?: () => void; // ✅ tambahin props callback
+}
+
+export default function Button({ text, icon = false, onClick }: ButtonProps) {
   return (
     <div className="bg-secondary p-1 rounded-full flex items-center gap-1 button-hover">
-      <button className="btn btn-primary font-medium rounded-full border-0 will-change-transform">
+      <button
+        onClick={onClick}
+        className="btn btn-primary font-medium rounded-full border-0 will-change-transform"
+      >
         {text}
       </button>
       {icon && <FaCircleArrowRight size="28" className="text-white " />}
