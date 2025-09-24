48adf5ddd6b0df89f61272dada5d6c8b6a3bc997
marmdhn <makbarramadhan62@gmail.com>
2025-09-24 15:46:56 +0700

    refactor: Update Footer styling and add new navigation links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,12 @@ import { FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
+    <footer className="footer footer-horizontal footer-center bg-gray-300 text-base-content rounded p-10">
       <nav className="grid grid-flow-col gap-4">
         <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
         <a className="link link-hover">Service</a>
+        <a className="link link-hover">Portfolio</a>
+        <a className="link link-hover">Pricing</a>
       </nav>
       <nav>
         <div className="grid grid-flow-col gap-4">
