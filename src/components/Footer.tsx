d3e7b47ef6ef15b691beb348686885434cea8e03
marmdhn <makbarramadhan62@gmail.com>
2025-09-24 14:55:43 +0700

    feat: Add Footer component with basic structure and styling

---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import { FaInstagram } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
+      <nav className="grid grid-flow-col gap-4">
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Service</a>
+      </nav>
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+          <FaInstagram size="28" className="text-primary button-hover" />
+        </div>
+      </nav>
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by{" "}
+          <span className="text-primary font-bold">AND Project</span>
+        </p>
+      </aside>
+    </footer>
+  );
+}
